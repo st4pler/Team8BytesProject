8f87256509b2c7fc5be58d5060a6fa5392f14f81
st4pler <st4pler@AcademyCity>
2016-02-11 12:40:13 +0800

    Ceniza, Jesse Neil

---
+++ src/Team8Bytes.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author st4pler
+ */
+public class Team8Bytes 
+{
+      public static int add(int a,int b)
+      {
+        return a + b;
+      }
+}
